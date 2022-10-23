ecb8cc24ce74b8cee45b51ec9c8e764c9eef88a9
Desktop <Desktop@DESKTOP-MQDOHGO>
2022-10-23 13:32:59 -0400

    Agregar archivos de proyecto.

---
+++ SuperMercado/Controllers/Carrito.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperMercado.Controllers
+{
+    public class Carrito : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SuperMercado/Models/Producto.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperMercado.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public double Precio { get; set; }
+
+        public string Imagen { get; set; }
+    }
+}
